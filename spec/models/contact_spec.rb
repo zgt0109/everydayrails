a0fb25ac28e36209c7d381fb4623ae1f7a1557ca
zgt0109 <2506526350@qq.com>
2016-12-12 15:14:33 +0800

    模型测试 加入describe context each简化测试

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -54,37 +54,33 @@ describe Contact do
     expect(contact.name).to eq 'John Doe'
   end
 
-  it "returns a sorted array of results that match" do
-    smith = Contact.create(
-      firstname: 'John',
-      lastname: 'Smith',
-      email: 'jsmith@example.com')
-    jones = Contact.create(
-      firstname: 'Tim',
-      lastname: 'Johes',
-      email: 'tjones@example.com')
-    johnson = Contact.create(
-      firstname: 'John',
-      lastname: 'Johnson',
-      email: 'jjohnson@example.com')
+    # validation examples ...
+  describe "filter last name by letter" do
+    before :each do
+      @smith = Contact.create(
+        firstname: 'John',
+        lastname: 'Smith',
+        email: 'jsmith@example.com')
+      @jones = Contact.create(
+        firstname: 'Tim',
+        lastname: 'Johes',
+        email: 'tjones@example.com')
+      @johnson = Contact.create(
+        firstname: 'John',
+        lastname: 'Johnson',
+        email: 'jjohnson@example.com')
+    end
 
-    expect(Contact.by_letter('J')).to eq [jones ,johnson]
-  end
+    context "with matching letters" do
+      it "returns a sorted array of results that match" do
+        expect(Contact.by_letter('J')).to eq [@jones ,@johnson]
+      end
+    end
 
-  it "omits results that do not match" do
-    smith = Contact.create(
-      firstname: 'John',
-      lastname: 'Smith',
-      email: 'jsmith@example.com')
-    jones = Contact.create(
-      firstname: 'Tim',
-      lastname: 'Johes',
-      email: 'tjones@example.com')
-    johnson = Contact.create(
-      firstname: 'John',
-      lastname: 'Johnson',
-      email: 'jjohnson@example.com')
-
-    expect(Contact.by_letter('J')).not_to include smith
+    context "with non-matching letters" do
+      it "omits results that do not match" do
+        expect(Contact.by_letter('J')).not_to include @smith
+      end
+    end
   end
 end
