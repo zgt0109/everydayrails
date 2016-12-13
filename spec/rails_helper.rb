770a335babca8f40cf8b9f561cc60ca6a3a58b92
zgt0109 <2506526350@qq.com>
2016-12-13 13:42:58 +0800

    add gem chromedriver-helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,11 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+Capybara.javascript_driver = :chrome
+Capybara.default_wait_time = 15
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
